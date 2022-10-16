e2b0569e61aef9588eca128068d224dad9ce8af6
Johan Vasquez <johanjvc7@gmail.com>
2022-10-16 15:52:04 -0500

    Spring construction
    initialize spring boot project

---
+++ Backend/AdmCR/src/test/java/com/example/AdmCR/AdmCrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdmCR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmCrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
